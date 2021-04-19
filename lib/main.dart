ae4dc51d6def12fbfddb4977e6a6ef104323b655
Darius Scorobete <42500339+DariusSKR@users.noreply.github.com>
2021-04-19 13:52:38 +0300

    MovieApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'layer.dart';
-import 'theme.dart';
+import 'home.dart';
 
-void main() => runApp(new MaterialApp(
+void main()  => runApp(new MaterialApp(
+    home:MovieListView(),));
 
-  home: LayerCalculator(),
-));
