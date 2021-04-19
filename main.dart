d3c3bb576526899096c0e81318e7ec1608d86eba
Darius Scorobete <42500339+DariusSKR@users.noreply.github.com>
2021-04-19 13:48:12 +0300

    Delete main.dart

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'layer.dart';
-import 'theme.dart';
-
-void main() => runApp(new MaterialApp(
-
-  home: LayerCalculator(),
-));
