bbe2aedc89f02bb3103123c71a98361d6ebd9c63
Darius Scorobete <42500339+DariusSKR@users.noreply.github.com>
2021-04-19 13:51:58 +0300

    Delete theme.dart

--- lib/theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-ThemeData lightThemeData(BuildContext context){
-  return ThemeData.light().copyWith(
-    primaryColor: Colors.black54,
-    scaffoldBackgroundColor: Colors.blue
-  );
-}
