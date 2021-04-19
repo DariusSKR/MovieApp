faa7417912b9cdf1f694735daf431ca309feb810
Darius Scorobete <42500339+DariusSKR@users.noreply.github.com>
2021-04-19 13:50:13 +0300

    BoundaryLayerCalculator

---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightThemeData(BuildContext context){
+  return ThemeData.light().copyWith(
+    primaryColor: Colors.black54,
+    scaffoldBackgroundColor: Colors.blue
+  );
+}
